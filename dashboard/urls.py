93d0c1f7cf3a7227e07a6f5b94633db98342620a
Seham Othman <210709718@posta.mu.edu.tr>
2026-04-28 15:42:43 +0300

    overview page designed by DJANGO

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # This means the "empty" path (homepage) runs the index function
     path('', views.index, name='index'),
+    path('diagnosis-support/', views.diagnosis_view, name='diagnosis_support'),
+    path('anamnesis-record/', views.anamnesis_view, name='anamnesis_record'),
+    path('model-management/', views.model_mgmt_view, name='model_management'),
+    path('hospital-management/', views.hospital_mgmt_view, name='hospital_management'),
+    path('security-traceability/', views.security_traceability_view, name='security_traceability'),
 ]
