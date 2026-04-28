11ee3b8105f91d3ecb8d13d485d5ce72960b5f23
Seham Othman <210709718@posta.mu.edu.tr>
2026-04-28 15:42:43 +0300

    overview page designed by DJANGO

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    # Django automatically looks in the 'templates' folder, 
-    # so we just give it the path AFTER that.
-    return render(request, 'dashboard/index.html')
+    return render(request, 'dashboard/Overview.html')
 
+def diagnosis_view(request):
+    return render(request, 'dashboard/diagnosis_support.html')
+
+def anamnesis_view(request):
+    return render(request, 'dashboard/Anamnesis_Record.html')
+
+def model_mgmt_view(request):
+    return render(request, 'dashboard/Model_Management.html')
+
+def hospital_mgmt_view(request):
+    return render(request, 'dashboard/Hospital_Management.html')
+
+def security_traceability_view(request):
+    return render(request, 'dashboard/Security_Traceability.html')
